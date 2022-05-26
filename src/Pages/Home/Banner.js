f83527c7d8790848fb3984388b395e4b8383be93
ASMRSAEED <asmrsaeed@gmail.com>
2022-05-26 12:39:14 +0600

    banner-updated

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -5,11 +5,11 @@ const Banner = () => {
         <div className='text-white '>
             <div className="hero min-h-screen bg-[#050326] pb-20">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img alt='Electric tools' src="https://i.ibb.co/RDqxhrH/alltools.webp" className="max-w-sm rounded-lg shadow-2xl" />
+                    <img alt='Electric tools' src="https://i.ibb.co/2gg32Rn/Banner.png" className="max-w-sm rounded-lg shadow-2xl" />
                     <div className='w-2/4'>
-                        <h1 className="text-5xl font-bold">Simple & Modern Electric <span className='text-yellow-500'>Hand Tools</span></h1>
-                        <p className="py-6 w-3/4">Electric Hand tools is most important in our life. When using hand tools then easy to Electric works So if you want Electric hand tools then get it.</p>
-                        <button className="btn btn-primary">Get Started</button>
+                        <h1 className="text-5xl font-bold">One Stop Tools <span className='text-red-500'>Station</span></h1>
+                        <p className="py-6 w-3/4">We do this by giving suppliers the tools necessary to reach a global audience for their products, and by helping buyers find products and suppliers quickly and efficiently.</p>
+                        <button className="btn btn-primary">Explore More</button>
                     </div>
                 </div>
             </div>
--- src/Pages/Tools/Tools.js
+++ src/Pages/Tools/Tools.js
@@ -1,4 +1,3 @@
-import React, { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Sheared/Loading';
 import Tool from './Tool';
