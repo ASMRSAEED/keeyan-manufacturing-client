402cbe41a539bfec8313d0c9ef958463b943b1d4
ASMRSAEED <asmrsaeed@gmail.com>
2022-05-26 14:28:16 +0600

    extra-section

--- src/Pages/Home/TrendingTools.js
+++ src/Pages/Home/TrendingTools.js
@@ -3,23 +3,23 @@ import React from 'react';
 const TrendingTools = () => {
     return (
         <div className='my-12'>
-            <h1 className='text-center  font-bold text-[#26264a] text-5xl'>Trending Tools</h1>
+            <h1 className='text-center  font-bold text-green-600 text-5xl'>More To Love</h1>
             <div className='grid w-3/4 mx-auto mt-8 lg:grid-cols-4 text-center text-xl'>
                 <div className='m-4'>
-                    <img className='border' src="https://image.made-in-china.com/318f0j00BQUYNEmMhDbo/video.jpg" alt="" />
-                    <p>115mm machine</p>
+                    <img className='border rounded-lg' src="https://i.ibb.co/7t1fppn/love-1.png" alt="" />
+                    <p>Knife Automatic Wire Cutter</p>
                 </div>
                 <div className='m-4'>
-                    <img className='border' src="https://www.technobuffalo.com/sites/technobuffalo.com/files/field/image/2020/02/worx-wx176l-switchdriver.jpg?itok=WgVTOjlY" alt="" />
-                    <p>Electric Drill</p>
+                    <img className='border rounded-lg ' src="https://i.ibb.co/0DgdDhG/love-2.png" alt="" />
+                    <p>Network Cable Lug Crimping</p>
                 </div>
                 <div className='m-4'>
-                    <img className='border' src="https://static-01.daraz.com.bd/p/dbcb3f8c9fdb5d1bccb2fb0e06b37d85.jpg" alt="" />
-                    <p>Soldering Iron</p>
+                    <img className='border rounded-lg' src="https://i.ibb.co/8NXL6rj/love-3.png" alt="" />
+                    <p>Folding Pliers Bottle Opener</p>
                 </div>
                 <div className='m-4'>
-                    <img className='border' src="https://image.made-in-china.com/318f0j00WEIfCLmhgDki/FHBN1006-Ali-mp4.jpg" alt="" />
-                    <p>Bent nose Plier</p>
+                    <img className='border rounded-lg' src="https://i.ibb.co/Cnh1k8J/love-4.png" alt="" />
+                    <p>Hose Clamp Removal Tool</p>
                 </div>
             </div>
         </div>
